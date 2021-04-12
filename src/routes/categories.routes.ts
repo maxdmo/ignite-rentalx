8a303dddf85e17b60d373437a115b7cac375bf49
Simarx Potásio <simarxpotasio@gmail.com>
2021-04-12 18:03:50 -0300

    Change categories route style.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
     categories.push({
+        id: uuidV4(),
         name,
         description,
     });
