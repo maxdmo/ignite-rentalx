f3e0b3f6abe94e926b5b03baf46388c2d67ba182
Simarx Potásio <simarxpotasio@gmail.com>
2021-04-12 17:55:18 -0300

    Initial Commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+    const { name, description } = request.body;
+
+    categories.push({
+        name,
+        description,
+    });
+
+    return response.status(201).send();
+});
+
+export { categoriesRoutes };
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+app.listen(3333, () => console.log("Server is running!"));
